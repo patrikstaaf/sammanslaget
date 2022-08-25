ea5ad35ab6f021ed67a5fb6bdfad84e0bd61d590
patrikstaaf <staaf.patrik@gmail.com>
2022-08-25 22:50:52 +0200

    Some cleanup + detect useragent device to remove fullscreen for ios

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        frutiger: ['Frutiger', 'sans'],
-      },
-      animation: {
-        jumpy: 'jump 1s infinite',
-      },
-      keyframes: {
-        jump: {
-          '0%, 100%': { transform: 'translateY(-25%)' },
-          '50%': { transform: 'translateY(0)' },
-        },
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
